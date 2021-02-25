f6f4e670c72a839a4aa2630effaed73528d9a5fc
Neo Jou <neojou@gmail.com>
2021-02-25 16:34:36 +0800

    Take off rtl8822cu_io.c
    Remove rtl8822cu_set_intf_ops
    
    Signed-off-by: Neo Jou <neojou@gmail.com>

--- rtl8822c.mk
+++ rtl8822c.mk
@@ -16,7 +16,6 @@ _HAL_INTFS_FILES +=	hal/rtl8822c/rtl8822c_halinit.o \
 ifeq ($(CONFIG_USB_HCI), y)
 _HAL_INTFS_FILES +=	hal/rtl8822c/$(HCI_NAME)/rtl8822cu_halinit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cu_halmac.o \
-			hal/rtl8822c/$(HCI_NAME)/rtl8822cu_io.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cu_xmit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cu_recv.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cu_led.o \
@@ -27,7 +26,6 @@ endif
 ifeq ($(CONFIG_PCI_HCI), y)
 _HAL_INTFS_FILES +=	hal/rtl8822c/$(HCI_NAME)/rtl8822ce_halinit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822ce_halmac.o \
-			hal/rtl8822c/$(HCI_NAME)/rtl8822ce_io.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822ce_xmit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822ce_recv.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822ce_led.o \
@@ -38,7 +36,6 @@ endif
 ifeq ($(CONFIG_SDIO_HCI), y)
 _HAL_INTFS_FILES +=	hal/rtl8822c/$(HCI_NAME)/rtl8822cs_halinit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cs_halmac.o \
-			hal/rtl8822c/$(HCI_NAME)/rtl8822cs_io.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cs_xmit.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cs_recv.o \
 			hal/rtl8822c/$(HCI_NAME)/rtl8822cs_led.o \
